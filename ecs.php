0374baa8a8299780f5284c44da6d23440b081f3d
TomasVotruba <tomas.vot@gmail.com>
2021-05-26 11:24:36 +0100

    update ECS and composer.json, allow PHP 7.2+

--- ecs.php
+++ ecs.php
@@ -11,18 +11,13 @@ use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
 return static function (ContainerConfigurator $containerConfigurator): void
 {
+    $containerConfigurator->import(SetList::COMMON);
+    $containerConfigurator->import(SetList::CLEAN_CODE);
+    $containerConfigurator->import(SetList::PSR_12);
+    $containerConfigurator->import(SetList::SYMPLIFY);
+
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/config', __DIR__ . '/ecs.php']);
-
-    $parameters->set(Option::SETS, [
-        SetList::COMMON,
-        SetList::CLEAN_CODE,
-        SetList::DEAD_CODE,
-        SetList::PSR_12,
-        SetList::PHP_70,
-        SetList::PHP_71,
-        SetList::SYMPLIFY,
-    ]);
 
     $parameters->set(Option::SKIP, [
         BlankLineAfterOpeningTagFixer::class => null,
